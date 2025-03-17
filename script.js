29ce61e2a5518cb0ba52dd74c156cb9d6f787a95
jeffmoney6 <buisnisman0@gmail.com>
2025-03-17 01:54:05 +0100

    Script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+// Hitta alla element med attributet 'data-lang'
 const elements = document.querySelectorAll('[data-lang]');
 
+// Visa den engelska versionen av innehållet
 elements.forEach(function (element) {
     const lang = element.getAttribute('data-lang');
     if (lang === 'en') {
         element.style.display = 'block';  // Visa engelsk text
     } else {
-        element.style.display = 'none';   // Dölj annan text
+        element.style.display = 'none';   // Dölj all annan text
     }
 });
